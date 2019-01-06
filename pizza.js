2dccdbcf55ef12101641f5831a1c8c9dbc81ce43
Duncan Arbman <duncanarbman@hotmail.com>
2019-01-06 14:45:20 +0100

    styling en java script

--- pizza.js
+++ pizza.js
@@ -1,1 +1,55 @@
-var toppings= ['salami','kaas','olijven','ui','paprika','ham'];
+var toppings=[
+    {name: 'salami',
+     price: '100',
+     choose: false},
+
+    {name:'kaas',
+     price:'100',
+     choose:false},
+
+    {name:'olijven',
+     price:'050',
+     choose:false},
+
+    {name:'ui',
+      price:'050',
+      choose:false},
+
+    {name:'paprika',
+     price:'050',
+     choose:false},
+
+    {name:'ham',
+     price:'1',
+     choose:false}];
+
+
+var pizzas=[{
+
+    pizzaId:'1',
+    name:'Pizza Salami',
+    image:'afb/pizza.jpg',
+    price:'750',
+},
+
+{   pizzaId:'2',
+    name:'Pizza Margherita',
+    image:'afb/margherita.jpg',
+    price:'600'},
+
+{   pizzaId:'3',
+    name:'Pizza Tonijn',
+    image:'afb/tonijn.jpg',
+    price:'600'}];
+
+
+
+function showPizza() {
+
+
+}
+
+
+
+
+
